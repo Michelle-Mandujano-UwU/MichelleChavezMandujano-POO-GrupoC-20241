68d9956394fda44a9b28319ddec755c5104bc437
Michelle-Mandujano-UwU <160373359+Michelle-Mandujano-UwU@users.noreply.github.com>
2024-04-23 23:15:28 -0600

    modific

---
+++ Libreria/src/usuarios/UsuarioEnSesion.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usuarios;
+
+/**
+ *
+ * @author Mich
+ */
+public class UsuarioEnSesion {
+     //realizar el registro cliente, libros y consulta de clientes y libros
+
+    /*
+     Iniciar sesion
+        validamos sus datos
+            1. si toso sale correcto
+                Mandar llamr a usuarioSesion
+                UsuarioEnSesion.setUsuarioActual(usuario)
+            2. Sino devolvemos un error
+
+
+        Editar datos
+            usuario = suarioensesion.obtenerinstancia()
+     */
+
+    private static UsuarioEnSesion instancia;
+    private Usuario usuarioActual;
+
+    private UsuarioEnSesion() {
+    }
+
+    public static UsuarioEnSesion obtenerInstancia() {
+        if(instancia == null) {
+            instancia = new UsuarioEnSesion();
+        }
+
+        return instancia;
+    }
+
+    public Usuario getUsuarioActual() {
+        return usuarioActual;
+    }
+
+    public void setUsuarioActual(Usuario usuarioActual) {
+        this.usuarioActual = usuarioActual;
+    }
+
+    public boolean hayUsuarioEnSesion() {
+        return usuarioActual != null;
+    }
+
+    public void cerrarSesion() {
+        instancia = null;
+        usuarioActual = null;
+    }
+}
